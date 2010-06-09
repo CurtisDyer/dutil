0a1b9caeff109826a29a95ae23e44376a17da8fb
Curtis Dyer <dyer85@gmail.com>
2010-06-09 00:46:49 -0700

    Forgot to remove git diff markings.

--- tests/inspect.c
+++ tests/inspect.c
@@ -37,11 +37,7 @@
  */
 enum { B, S, I, D, STR, P, ARR };
 
-<<<<<<< HEAD
 static struct data {
-=======
-struct data {
->>>>>>> d7e57a9b5358265a322dc44ff1812ce1c3f1b968
 	unsigned char b;
 	short s;
 	unsigned i;
@@ -49,13 +45,10 @@ struct data {
 	char str[20];
 	char *p;
 	int arr[5];
-<<<<<<< HEAD
 } foo = {
 	'B', 934, UINT_MAX-100, 3.14159265358979,
 	"Hello, world!", "Hello, world!",
 	{ 1, 1, 2, 3, 5 }
-=======
->>>>>>> d7e57a9b5358265a322dc44ff1812ce1c3f1b968
 };
 
 void intro(void)
@@ -70,12 +63,9 @@ void intro(void)
 	puts("a file as a hexdump.\n");
 }
 
-<<<<<<< HEAD
 #define COLS	8
 #define WIDTH	4
 
-=======
->>>>>>> d7e57a9b5358265a322dc44ff1812ce1c3f1b968
 int main(void)
 {
 	size_t i = 0, len;
@@ -85,29 +75,16 @@ int main(void)
 		"char str[20]", "char *p",
 		"int arr[5]"
 	};
-<<<<<<< HEAD
-=======
-	struct data foo = {
-		'B', 934, UINT_MAX-100, 3.14159265358979,
-		"Hello, world!", "Hello, world!",
-		{ 1, 1, 2, 3, 5 }
-	};
->>>>>>> d7e57a9b5358265a322dc44ff1812ce1c3f1b968
 
 	intro();
 
 	puts("struct data foo:");
-<<<<<<< HEAD
 	vardump(&foo, sizeof foo, COLS, WIDTH);
-=======
-	vardump(&foo, sizeof foo);
->>>>>>> d7e57a9b5358265a322dc44ff1812ce1c3f1b968
 	putchar('\n');
 
 	len = sizeof member / sizeof member[0];
 	while (i < len) {
 		printf("%s:\n", member[i]);
-<<<<<<< HEAD
 		switch (i++) {
 		case B:		vardump(&foo.b,  sizeof foo.b, COLS, WIDTH);   break;
 		case S:		vardump(&foo.s,  sizeof foo.s, COLS, WIDTH);   break;
@@ -124,21 +101,6 @@ int main(void)
 	vardump(NULL, sizeof(void*), COLS, WIDTH);
 	putchar('\n');
 
-=======
-		switch (i) {
-			case B:		vardump(&foo.b,  sizeof foo.b);   break;
-			case S:		vardump(&foo.s,  sizeof foo.s);   break;
-			case I:		vardump(&foo.i,  sizeof foo.i);   break;
-			case D:		vardump(&foo.d,  sizeof foo.d);   break;
-			case STR:	vardump(foo.str, sizeof foo.str); break;
-			case P:		vardump(&foo.p,  sizeof foo.p);   break;
-			case ARR:	vardump(foo.arr, sizeof foo.arr); break;
-		}
-		++i;
-		putchar('\n');
-	}
-
->>>>>>> d7e57a9b5358265a322dc44ff1812ce1c3f1b968
 	return 0;
 }
 
